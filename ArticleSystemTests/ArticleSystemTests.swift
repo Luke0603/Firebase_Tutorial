bdc814424b0c36a894296fc52a89faeaeae3a589
Luke0603 <bert0603@gmail.com>
2019-08-08 14:44:35 +0800

    Initial Commit

---
+++ ArticleSystemTests/ArticleSystemTests.swift
@@ -0,0 +1,34 @@
+//
+//  ArticleSystemTests.swift
+//  ArticleSystemTests
+//
+//  Created by 陳博竣 on 2019/8/8.
+//  Copyright © 2019 Luke. All rights reserved.
+//
+
+import XCTest
+@testable import ArticleSystem
+
+class ArticleSystemTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
